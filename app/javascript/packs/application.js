e938b61b2bc85f1dc51628a94c711e34a64813d0
gustavo <gustavo26cs@gmail.com>
2020-09-07 13:54:14 -0300

    Fixed index page styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("bootstrap-sprockets")
-require("masonry/transitions")
-require("masonry/jquery.masonry")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
